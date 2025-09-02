9653cffeb29c5967f52aee4fc4585040ce7b501b
Rutwik <rutwikpatel08@gmail.com>
2025-09-02 12:32:31 +0530

    Try google div login

--- src/features/auth/components/GoogleLogin.tsx
+++ src/features/auth/components/GoogleLogin.tsx
@@ -1,10 +1,23 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { CardContent } from '../../../components/ui/card';
+import { useEffect } from 'react';
 
 export function GoogleLoginComponent({ handleLoginSuccess, handleLoginError }: { handleLoginSuccess: (response: any) => void, handleLoginError: (error: any) => void }) {
+    useEffect(() => {
+        // Expose the React handler to the global window object
+        (window as any).handleDivManualLoginSuccess = (response: any) => {
+            try {
+                handleLoginSuccess(response);
+            } catch (err) {
+                handleLoginError(err);
+            }
+        };
+
+    }, []);
+
     return (
         <CardContent className="space-y-4">
-            <GoogleLogin
+            {/* <GoogleLogin
                 onSuccess={handleLoginSuccess}
                 onError={() => handleLoginError("Login failed, please try again.")}
                 containerProps={
@@ -22,7 +35,27 @@ export function GoogleLoginComponent({ handleLoginSuccess, handleLoginError }: {
                 theme="outline"
                 shape="rectangular"
                 size="large"
-            />
+            /> */}
+            <div id="g_id_onload"
+                data-client_id="201954194593-36t0nksh9jusg01k58et81ct27objt26.apps.googleusercontent.com"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="handleDivManualLoginSuccess"
+                data-nonce=""
+                data-auto_select="true"
+                data-itp_support="true">
+            </div>
+
+            <div className="g_id_signin"
+                data-type="standard"
+                data-shape="pill"
+                data-theme="filled_blue"
+                data-text="signin_with"
+                data-size="large"
+                data-locale="en-US"
+                data-logo_alignment="left"
+                data-width="300px">
+            </div>
         </CardContent>
     )
 }
